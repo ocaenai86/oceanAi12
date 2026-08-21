7adf32c72c856c282999fbf852c6d775e0ffba37
ocaenai86 <ocaenai.86@gmail.com>
2026-08-22 00:08:54 +0330

    Initialize React app with styles and dark mode

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,1 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
+import "./styles/main.css";
+import "./styles/studio.css";
+import "./styles/studio-dark.css";
+import "./styles/studio-light.css";
+
+document.body.classList.add("oceanai-studio-dark");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

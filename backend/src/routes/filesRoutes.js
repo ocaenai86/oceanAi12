ceb1f07b49669bd1a6fd2c2a3bcb64cd6e13748d
ocaenai86 <ocaenai.86@gmail.com>
2026-08-21 11:48:43 +0330

    Update filesRoutes.js

--- backend/src/routes/filesRoutes.js
+++ backend/src/routes/filesRoutes.js
@@ -1,1 +1,8 @@
+import express from 'express';
+import { saveProject } from '../controllers/filesController.js';
 
+const router = express.Router();
+
+router.post('/save', saveProject);
+
+export default router;

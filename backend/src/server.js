aced396844faee36f37bf7f4433d681c675edb61
ocaenai86 <ocaenai.86@gmail.com>
2026-08-21 11:49:41 +0330

    Set up Express server with files routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,9 @@
+import express from 'express';
+import filesRoutes from './routes/filesRoutes.js';
 
+const app = express();
+app.use(express.json());
+
+app.use('/api/files', filesRoutes);
+
+app.listen(3000, () => console.log('Backend running on port 3000'));
